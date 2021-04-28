446c1d4a2994c206c349f9a6c4ff608e0823af74
Валентин Дудин <dudinvalentin@gmail.com>
2021-04-29 03:21:46 +1000

    lab5 from rhonda

---
+++ rhonda/rhonda.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stdio.h>
+using  namespace  std;
+int main()
+{
+	int N;
+	bool lastLetter = true;
+
+	FILE* in;
+	fopen_s(&in, "in.txt", "rt");
+	fscanf_s(in, "%d", &N);
+	fclose(in);
+
+	FILE* out;
+	fopen_s(&out, "out.txt", "w");
+
+	for (int i = 2; i <= N; i++)
+	{
+		bool isPrime = true;
+		for (int k = 2; k < i; k++)
+		{
+			if (i % k == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime)
+		{
+			if (lastLetter)
+			{
+				cout << i;
+				lastLetter = false;
+				fprintf_s(out, "%d", i);
+			}
+			else
+			{
+				cout << ", " << i;
+				fprintf_s(out, ", %d", i);
+			}
+		}
+	}
+	fclose(out);
+}
