d8bbbd22e7f0e1c60b7dac7c884b59b5d2c41091
Валентин Дудин <dudinvalentin@gmail.com>
2021-04-29 19:21:07 +1000

    update lin

--- rhonda/rhonda.cpp
+++ rhonda/rhonda.cpp
@@ -7,12 +7,12 @@ int main()
 	bool lastDigit = true;
 
 	FILE* in;
-	fopen_s(&in, "in.txt", "rt");
-	fscanf_s(in, "%d", &entNum);
+	in = fopen("/media/sd0/in.txt", "rt");
+	fscanf(in, "%d", &entNum);
 	fclose(in);
 
 	FILE* out;
-	fopen_s(&out, "out.txt", "w");
+	out = fopen("/media/sd0/out.txt", "w");
 
 	for (int i = 2; i <= entNum; i++)
 	{
@@ -30,10 +30,10 @@ int main()
 			if (lastDigit)
 			{
 				lastDigit = false;
-				fprintf_s(out, "%d", i);
+				fprintf(out, "%d", i);
 			}
 			else
-				fprintf_s(out, ", %d", i);
+				fprintf(out, ", %d", i);
 		}
 	}
 	fclose(out);
